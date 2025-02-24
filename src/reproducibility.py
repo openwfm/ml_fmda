42a4a1ded4bfd1ba6f8db95c89c4677edf3e014d
jonathonh <jonathon.hirschi@ucdenver.edu>
2025-02-24 11:55:51 -0700

    update test, save data to test directory

--- src/reproducibility.py
+++ src/reproducibility.py
@@ -4,25 +4,53 @@
 import os
 import random
 import numpy as np
-import tensorflow as tf
-environ={'TF_DETERMINISTIC_OPS':'1','PYTHONHASHSEED':'0','TF_CPP_MIN_LOG_LEVEL':'2'}
-# print('setting',environ)
+
+# Attempt to import TensorFlow and related functions.
+try:
+    import tensorflow as tf
+    _tf_available = True
+except ImportError:
+    _tf_available = False
+
+# Set common environment variables.
+environ = {
+    'TF_DETERMINISTIC_OPS': '1',
+    'PYTHONHASHSEED': '0',
+    'TF_CPP_MIN_LOG_LEVEL': '2'
+}
 os.environ.update(environ)
+
 def set_seed(seed=123):
     """
     Set reproducibility seeds in several places.
     
+    If TensorFlow is installed, it sets TensorFlow-related seeds and 
+    enables deterministic operations. Otherwise, it only sets seeds for
+    Python's random module and NumPy.
+    
     Example:
     --------
-    >>> set_seed()
+    >>> set_seed(42)
     """
+    # Seed Python's random module.
     random.seed(seed)
+    # Seed NumPy.
     np.random.seed(seed)
+    # Update environment variable for PYTHONHASHSEED.
     os.environ['PYTHONHASHSEED'] = str(seed)
-    tf.random.set_seed(seed)
-    tf.keras.utils.set_random_seed(seed)
-    tf.config.experimental.enable_op_determinism()
-    print('resetting random seeds to %i' % seed)
+
+    if _tf_available:
+        # Set TensorFlow seeds.
+        tf.random.set_seed(seed)
+        # Some TF versions have this additional utility.
+        if hasattr(tf.keras.utils, 'set_random_seed'):
+            tf.keras.utils.set_random_seed(seed)
+        # Enable deterministic operations if available.
+        if hasattr(tf.config.experimental, 'enable_op_determinism'):
+            tf.config.experimental.enable_op_determinism()
+    
+    print('Resetting random seeds to %i' % seed)
+
 
 
 
