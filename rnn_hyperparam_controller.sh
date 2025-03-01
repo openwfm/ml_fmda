a490a536d4254a9bf95c2f517500d60b23944e50
jonathonh <jonathon.hirschi@ucdenver.edu>
2025-03-01 10:15:17 -0700

    Split controller into 2 for the 2 step hyperparam tuning. TODO: make one script and figure out waiting for process to finish

--- rnn_hyperparam_controller.sh
+++ rnn_hyperparam_controller.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#SBATCH --job-name=rnn_hyperparam
+#SBATCH --output=logs/rnn_hyperparam_%a.out
+#SBATCH --ntasks=1
+#SBATCH --partition=math-alderaan-short
+
+
+
 # Script used to run the hyperparam tuning process. 
 # Steps: 
 # 1. Run rnn_hyperparam_setup.py to create a directory and test files 
@@ -21,14 +28,30 @@ source ~/.bashrc
 conda activate ml_fmda_models
 
 # Run setup, specify <model_directory> and <data_directory>
-mkdir "$MODEL_DIRECTORY"
-mkdir "$MODEL_DIRECTORY/model_errors"
+mkdir -p "$MODEL_DIRECTORY"
+mkdir -p "$MODEL_DIRECTORY/model_errors"
 python src/rnn_hyperparam_setup.py "$MODEL_DIRECTORY" "$DATA_DIRECTORY"
 
 # Run Model Architecture tuning
 # Extract number of models from previous step to setup array
 # Create log directory and send outputs there
-mkdir "$MODEL_DIRECTORY/logs"
+mkdir -p "$MODEL_DIRECTORY/logs"
 N_MODELS=$(wc -l < "$MODEL_DIRECTORY/model_grid.txt")
-sbatch --array=1-$N_MODELS --output="$MODEL_DIRECTORY/logs/rnn_hyperparam_%a.out" run_rnn_hyperparam_model.sh "$MODEL_DIRECTORY"
+sbatch --array=1-$N_MODELS --output="$MODEL_DIRECTORY/logs/model_%a.out" run_rnn_hyperparam_model.sh "$MODEL_DIRECTORY"
+
+
+
+
+# TODO: add code from controller 2 below, while waiting for sbatch above to finish
+
+# Analyze Model Error, extract minimum error model
+# python src/rnn_hyperparam_eval.py "$MODEL_DIRECTORY"
+
+
+
+# Run Optimization parameter tuning
+# Extract number of opt configurations from setup
+#mkdir -p "$MODEL_DIRECTORY/opt_errors"
+#N_OPT=$(wc -l < "$MODEL_DIRECTORY/opt_grid.txt")
+# sbatch --array=1-$N_OPT --output="$MODEL_DIRECTORY/logs/opt_%a.out" run_rnn_hyperparam_opt.sh "$MODEL_DIRECTORY" 
 
