f2a880b6e2d08fc4f9251af3a19a2ea0ceb26596
jonathonh <jonathon.hirschi@ucdenver.edu>
2025-03-05 15:38:37 -0700

    update hyperparam control scripts with wait times. Worked on small test

--- rnn_hyperparam_controller1.sh
+++ rnn_hyperparam_controller1.sh
@@ -15,13 +15,14 @@
 
 
 # Setup input arg, directory where model and outputs live
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Error: Expected exactly 2 arguments, but got $#."
-    echo "Usage: $0 <model_directory> <data_directory>"
+    echo "Usage: $0 <model_directory> <data_directory> <config_file>"
     exit 1
 fi
 MODEL_DIRECTORY="$1"
 DATA_DIRECTORY="$2"
+CONFIG_FILE="$3"
 
 # Set up environment
 source ~/.bashrc
@@ -31,7 +32,7 @@ conda activate ml_fmda_models
 echo "Running Hyperparam Tuning step 1, model architecture"
 mkdir -p "$MODEL_DIRECTORY"
 mkdir -p "$MODEL_DIRECTORY/model_errors"
-#python src/rnn_hyperparam_setup.py "$MODEL_DIRECTORY" "$DATA_DIRECTORY"
+python src/rnn_hyperparam_setup.py "$MODEL_DIRECTORY" "$DATA_DIRECTORY" "$CONFIG_FILE"
 
 # Run Model Architecture tuning
 # Extract number of models from previous step to setup array
@@ -39,7 +40,7 @@ mkdir -p "$MODEL_DIRECTORY/model_errors"
 # Extract the jobid to use with waiting for the next things to run
 mkdir -p "$MODEL_DIRECTORY/logs"
 N_MODELS=$(wc -l < "$MODEL_DIRECTORY/model_grid.txt")
-job1_id=$(sbatch --array=1-$N_MODELS --output="$MODEL_DIRECTORY/logs/model_%a_%j.out" run_rnn_hyperparam_model.sh "$MODEL_DIRECTORY" |  awk '{print $NF}')
+job1_id=$(sbatch --array=1-$N_MODELS --output="$MODEL_DIRECTORY/logs/model_%j_%a.out" run_rnn_hyperparam_model.sh "$MODEL_DIRECTORY" |  awk '{print $NF}')
 
 
 
