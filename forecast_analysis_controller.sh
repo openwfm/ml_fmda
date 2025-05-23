c9a5e998a34f0d573902aef553f4da811eecd123
jonathonh <jonathon.hirschi@ucdenver.edu>
2025-05-23 13:02:20 -0600

    Rename forecast files with 'analysis' rather than 'period' for clarity

--- forecast_analysis_controller.sh
+++ forecast_analysis_controller.sh
@@ -13,14 +13,13 @@
 
 
 # Setup input arg, directory where model and outputs live
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Error: Expected exactly 2 arguments, but got $#."
-    echo "Usage: $0 <forecast_directory> <data_directory> <config_path>"
+    echo "Usage: $0 <forecast_directory> <config_path>"
     exit 1
 fi
 FORECAST_DIRECTORY="$1"
-DATA_DIRECTORY="$2"
-CONFIG_PATH="$3"
+CONFIG_PATH="$2"
 
 # Set up environment
 source ~/.bashrc
@@ -30,13 +29,13 @@ conda activate ml_fmda_models
 mkdir -p "$FORECAST_DIRECTORY"
 mkdir -p "$FORECAST_DIRECTORY/forecast_periods"
 mkdir -p "$FORECAST_DIRECTORY/logs"
-python src/forecast_analysis_setup.py "$FORECAST_DIRECTORY" "$DATA_DIRECTORY" "$CONFIG_PATH"
+python src/forecast_analysis_setup.py "$FORECAST_DIRECTORY" "$CONFIG_PATH"
 
 
-# Run train/test for each forecast period
-# Create slurm job array for each forecast period
-N_PERIOD=$(jq '.forecast_periods' forecasts/fmc_forecast_test/analysis_info.json)
-job_id=$(sbatch --array=1-$N_PERIOD --mem=8G --output="$FORECAST_DIRECTORY/logs/fperiod_%j_%a.out" run_forecast_period.sh "$FORECAST_DIRECTORY")
+# Run train/test for each forecast replication
+# Create slurm job array for each forecast replication
+N_REPS=$(jq '.nreps' "$FORECAST_DIRECTORY/analysis_info.json")
+job_id=$(sbatch --array=1-$N_REPS --mem=8G --output="$FORECAST_DIRECTORY/logs/frep_%j_%a.out" run_forecast_analysis.sh "$FORECAST_DIRECTORY")
 
 # Wait for job to finish and run eval
 #while squeue -j "$job_id" &>/dev/null; do
