78d439f890d0d3004183dc8001575b964d247d88
jonathonh <jonathon.hirschi@ucdenver.edu>
2025-07-09 09:32:01 -0600

    Update forecast scripts, better read and eval script

--- src/forecast_eval.py
+++ src/forecast_eval.py
@@ -21,20 +21,22 @@ CONFIG_DIR = osp.join(PROJECT_ROOT, "etc")
 # Read Project Module Code
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from utils import Dict, read_pkl, read_yml, str2time, time_range
-import data_funcs
+
 
 # Module Code
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def read_hdf_list(file_list, key):
-    """
-    Given a list of hdf files and a dataset key, read all with pandas and merge
-    
-    NOTE: assumes certain naming structure for file names and  the line that adds the column "rep"
-    """
-    data = [pd.read_hdf(osp.join(f_dir, "forecast_outputs", f), key=key).assign(rep=int(re.search(r'_(\d+)\.h5$', f).group(1))) for f in files]
-    data = pd.concat(data, ignore_index=True)
-    return data
+    all_data = []
+    for f in files:
+        f_path = osp.join(f_dir, "forecast_outputs", f)
+        rep = int(re.search(r'_(\d+)\.h5$', f).group(1))
+        for key in key_list:
+            df = pd.read_hdf(f_path, key=key)
+            df = df.assign(rep=rep, Model=key)
+            df = calc_errs(df) # add residuals 
+            all_data.append(df)
+    return pd.concat(all_data, ignore_index=True)
 
 def calc_errs(df, pred_col="preds", true_col="fm"):
     """
@@ -81,11 +83,6 @@ def summary_table(df, group_vars, bound_vars="rep"):
     #return summary_formatted
     return summary_stats
 
-# Dictionary used to calculate error summary stats, based on given grouping
-agg_named = {
-    'mean_error': pd.NamedAgg(column='residual', aggfunc='mean'),
-    'mean_squared_error': pd.NamedAgg(column='squared_error', aggfunc='mean'),
-}
 
 # Executed Code
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -99,6 +96,7 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     f_dir = sys.argv[1]
+    print(f"Evaluating forecast accuracy for forecast run in directory: {f_dir}")
     fconf = Dict(read_yml(osp.join(f_dir, "forecast_config.yaml")))
     out_dir = osp.join(f_dir, "error_analysis")
     os.makedirs(out_dir, exist_ok=True)
@@ -109,18 +107,20 @@ if __name__ == '__main__':
     files = os.listdir(osp.join(f_dir, 'forecast_outputs'))
     files = sorted(files, key=lambda x: int(re.search(r'_(\d+)\.h5$', x).group(1))) # Sort by task number, shouldn't be necessary but for clarity
     ## Read and combine into dataframe, add indicator column for replication number from file name
-    rnn = read_hdf_list(files, key="rnn")
-    ode = read_hdf_list(files, key="ode")
-    xgb = read_hdf_list(files, key="xgb")
-    clim = read_hdf_list(files, key="clim")
-    clim = clim[(~clim.preds.isna()) & (~clim.fm.isna())]
+    key_list =["rnn", "ode", "xgb", "clim"]
+    df = read_hdf_list(files, key_list)
+    #rnn = read_hdf_list(files, key="rnn")
+    #ode = read_hdf_list(files, key="ode")
+    #xgb = read_hdf_list(files, key="xgb")
+    #clim = read_hdf_list(files, key="clim")
+    #clim = clim[(~clim.preds.isna()) & (~clim.fm.isna())]
 
     # Evaluate Accuracy
     ## First calculate errors (residuals)
-    rnn = calc_errs(rnn); rnn["Model"] = "RNN"
-    ode = calc_errs(ode); ode["Model"] = "ODE"
-    xgb = calc_errs(xgb); xgb["Model"] = "XGB"
-    clim = calc_errs(clim); clim["Model"] = "CLIMATOLOGY"
+    #rnn = calc_errs(rnn); rnn["Model"] = "RNN"
+    #ode = calc_errs(ode); ode["Model"] = "ODE"
+    #xgb = calc_errs(xgb); xgb["Model"] = "XGB"
+    #clim = calc_errs(clim); clim["Model"] = "CLIMATOLOGY"
 
     ## Combine all individual predictions so someone can easily reproduce summary tables that aggregate
     ## Add spatial info and calculate hour of dayi
@@ -128,14 +128,34 @@ if __name__ == '__main__':
     print(f"    {fconf.f_start=}")
     print(f"    {fconf.f_end=}")
 
-    df = pd.concat([rnn, xgb, ode, clim], ignore_index=True)
+    # df = pd.concat([rnn, xgb, ode, clim], ignore_index=True)
+    
+    # df.to_hdf(osp.join(f_dir, "all_errors.h5"), key="all_errors") # too large to write with hdf
+
+    
     ml_dict = read_pkl(osp.join(f_dir, "ml_data.pkl"))
     loc_df = pd.DataFrame.from_dict(
         {k: v["loc"] for k, v in ml_dict.items()},
         orient="index"
     )
-    del ml_dict
+    # Add overall predictor metrics
+    df2 = pd.concat(
+        [v["data"][fconf.features_list] for k, v in ml_dict.items()],
+        ignore_index=True        
+    )
+    psummary = pd.DataFrame({
+        "Variable": df2.columns,
+        "Mean": df2.mean().values,
+        "Low": df2.min().values,
+        "High": df2.max().values
+    })   
+    print(f"Writing summary of all variables to: {osp.join(out_dir, 'all_variables_summary.csv')}")
+    psummary.to_csv(osp.join(out_dir, "all_variables_summary.csv"), index=False)
+    del ml_dict; del df2
 
+
+    # Write df of RNN errors for more granular analysis
+    df[df.Model == "rnn"].to_csv(osp.join(out_dir, "rnn_preds.csv"), index=False)
 
     # Data very big to write as h5, and only done as external double check on calculations. 
     # Use fperiod files directly for reproducing error calcs
@@ -146,20 +166,25 @@ if __name__ == '__main__':
     ## Overall Error, averaged over every hour, location, and replication
     ## Bounds from +/- 1std for replications
     table1 = summary_table(df, group_vars = ["Model", "rep"], bound_vars ="rep") 
+    print(f"Writing overall error summary to {osp.join(out_dir, 'overall.csv')}")
     table1.to_csv(osp.join(out_dir, "overall.csv"), index=False)
 
     ## Error by Station, averaged over all times and replications. Bounds from reps
     table_st = summary_table(df, group_vars = ["Model", "stid", "rep"], bound_vars ="rep")
+    print(f"Writing by station error summary to {osp.join(out_dir, 'by_stid.csv')}")
     table_st.to_csv(osp.join(out_dir, "by_stid.csv"), index=False)
     ## Error by hour of day (0-23)
     ## averaged over all stations and days and reps, fixed hour 0 at 00:00 UTC
     df["hod"] = pd.to_datetime(df.date_time).dt.hour
     table_hod = summary_table(df, group_vars = ["Model", "hod", "rep"], bound_vars ="rep")       
+    print(f"Writing by hour of day error summary to {osp.join(out_dir, 'by_hod.csv')}")
     table_hod.to_csv(osp.join(out_dir, "by_hod.csv"), index=False)
 
     ## Error by hour & day, averaged over all stations and reps
     table_t = summary_table(df, group_vars = ["Model", "date_time", "rep"], bound_vars ="rep")
+    print(f"Writing per date time error summary to {osp.join(out_dir, 'by_dt.csv')}")
     table_t.to_csv(osp.join(out_dir, "by_dt.csv"), index=False)
+
 
 
 
