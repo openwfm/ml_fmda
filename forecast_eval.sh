fa6f06f1bf528ed888a8cdf78dfddae0f88c8150
jonathonh <jonathon.hirschi@ucdenver.edu>
2025-08-28 16:49:37 -0600

    Add shell file for forecast error analysis summary script, NOTE: not sure if we want this moving forward

---
+++ forecast_eval.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=feval
+#SBATCH --output=logs/forecast_eval_%j.out
+#SBATCH --ntasks=1
+#SBATCH --partition=math-alderaan
+#SBATCH --mem=8G
+
+# Shell script to combine model outputs and calculate errors for each replication in input model directory
+# Credit to user lorellis for structure of the code
+# Re-grab the variable that was passed in the run_rnn_hyperparam_model.sh script
+
+
+MODEL_DIRECTORY=$1
+
+# Set up environment
+source ~/.bashrc
+conda activate ml_fmda_models
+
+# Pass array number to python script and run
+
+python src/forecast_eval.py $MODEL_DIRECTORY
+
+
