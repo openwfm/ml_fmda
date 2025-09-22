973a3eda9b6cb6d6b43b57fe0e863917ec260cfd
jonathonh <jonathon.hirschi@ucdenver.edu>
2025-09-22 10:29:46 -0600

    shell file for training on cpu, just points to normal partition

---
+++ train_cpu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+#SBATCH --job-name=train
+#SBATCH --partition=math-alderaan-short
+#SBATCH --output=logs/train_%j.out
+#SBATCH --ntasks=2
+#SBATCH --mem=32G
+
+# Credit to user lorellis for aspects of this code structure
+
+if [ "$#" -ne 2 ]; then
+    echo "Error: Expected exactly 2 arguments, but got $#."
+    echo "Usage: $0 <model_directory> <config_path>"
+    exit 1
+fi
+
+MODEL_DIRECTORY="$1"
+CONFIG_PATH="$2"
+
+echo "Model directory: $MODEL_DIRECTORY"
+echo "Config path: $CONFIG_PATH"
+
+# Set up environment
+source ~/.bashrc
+conda activate gpu_TEST
+
+python src/train.py "$MODEL_DIRECTORY" "$CONFIG_PATH"
