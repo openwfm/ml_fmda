74b238535bac61fab04d50b8834827808b8dd4d4
jonathonh <jonathon.hirschi@ucdenver.edu>
2025-10-09 10:03:09 -0600

    add API test

--- tests/test_ingest_RAWS.py
+++ tests/test_ingest_RAWS.py
@@ -7,6 +7,7 @@ import sys
 import warnings
 import numpy as np
 import pandas as pd
+from datetime import datetime, timezone, timedelta
 
 # Set up project paths
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -22,9 +23,14 @@ import ingest.RAWS as rr
 
 bbox = [42, -105, 45, -100]
 
-# Expected Retrieved Stations (As of Oct 9, 2025)
+# Stash Test: Expected Retrieved Stations (As of Oct 9, 2025)
 stids = ['BRLW4', 'HRSN1', 'DOHS2', 'BKFS2', 'CRRS2', 'NMOS2', 'RDCS2', 'VRFN1', 'PINS2', 'DVLW4', 'WCAS2', 'RHRS2', 'TS485', 'WPKS2', 'CSPS2', 'SDSS2', 'RWES2', 'MTRN1', 'MKVN1', 'SFRS2', 'TT591']
 fm_hash_expected = "ea28f3c6929ccc5574b8ed3587cb3759"
+
+# API Test: Expected Retrieved Stations (As of Oct 9, 2025)
+stids2 = ['BRLW4', 'HRSN1', 'DOHS2', 'BKFS2', 'CRRS2', 'NMOS2', 'RDCS2', 'VRFN1', 'PINS2', 'DVLW4', 'TS399', 'AGTN1', 'WCAS2', 'RHRS2', 'TS485', 'WPKS2', 'CSPS2', 'SDSS2', 'RWES2', 'TT456', 'MTRN1', 'MKVN1', 'SFRS2']
+fm_hash_expected2="3d249ee136c1c5633a2526d022e6e982"
+
 
 if __name__ == '__main__':
 
@@ -43,6 +49,30 @@ if __name__ == '__main__':
         warnings.warn("FM hash doesn't match expected")
         failed=True
     if not failed:
-        print(f"TEST PASSED: stashed RAWS dictionary matches expected stations, FM data hash matches") 
+        print(f"TEST PASSED STASH: stashed RAWS dictionary matches expected stations, FM data hash matches") 
     
+    print()
+    print(f"Testing RAWS Retrieval from API")
+    start = str2time('2024-12-30T00:00:00Z')
+    end = str2time('2024-12-30T02:00:00Z')
+    
+    oneyr = datetime.now(timezone.utc) - timedelta(days=365)
+    if start <= oneyr:
+        warnings.warn("Start date not within the past year, free Synoptic API will fail")
+
+    raws_dict = rr.build_raws_dict_api(start, end, bbox)
+    fm = pd.concat([raws_dict[st]["RAWS"]["fm"] for st in raws_dict]).reset_index(drop=True).astype(float).to_numpy().round(3)    
+    fm_hash = hash_ndarray(fm)
+
+    failed=False
+    if [*raws_dict.keys()] != stids2:
+        warnings.warn("Returned STIDs don't match expected")
+        failed=True
+    if fm_hash != fm_hash_expected2:
+        warnings.warn("FM hash doesn't match expected")
+        failed=True
+    if not failed:
+        print(f"TESTS PASSED API: RAWS dictionary matches expected stations, FM data hash matches")    
+
+
 
