0ff9b2588ff2e414d9ddf7e33b038d6e16149243
jh-206 <jhirschi206@gmail.com>
2025-11-01 17:58:24 -0400

    one more file from predictors branch, associated shell file

--- build_fmda_data.sh
+++ build_fmda_data.sh
@@ -15,23 +15,14 @@
 # NOTE: intended to use with GACC bounding boxes, see rtma_cycler in wrfxpy for coords, but it should work with any bbox
 # NOTE: executed python script organizes output by days. Minimum saved data is one full day, so it will pad out if only an hour is requested
 
-if [ "$#" -ne 4 ]; then
-    echo "Error: Expected exactly 4 arguments, but got $#."
-    echo "Usage: $0 <start_time_utc> <end_time_utc> <bbox> <target_directory>"
-    echo "Example: $0 '2023-01-01T00:00:00Z' '2023-01-01T02:00:00Z' '[42,-124.6,49,-116.4]' data/nw_fmda"
+if [ "$#" -ne 1 ]; then
+    echo "Error: Expected exactly 1 arguments, but got $#."
+    echo "Usage: $0 <config_file>"
+    echo "Example: $0 etc/forecast_analysis_TEST.yaml"
     exit 1
 fi
 
-START_TIME="$1"
-END_TIME="$2"
-BBOX="$3"
-DIR="$4"
-
-echo "Building data for FMDA. Run configuration:"
-echo "Start Time: $START_TIME"
-echo "End Time: $END_TIME"
-echo "Bounding Box: $BBOX"
-echo "Target Data Directory: $DIR"
+CONFIG_FILE="$1"
 
 
 # Set up environment
@@ -39,6 +30,6 @@ source ~/.bashrc
 conda activate ml_fmda_data
 
 export PYTHONUNBUFFERED=1
-python -u src/ingest/get_fmda_data.py "$START_TIME" "$END_TIME" "$BBOX" "$DIR"
+python -u src/ingest/get_fmda_data.py "$CONFIG_FILE"
 
 
